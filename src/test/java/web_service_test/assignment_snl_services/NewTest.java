6c9f40bab3d4e6bebcc6f4d4218419e4348b00fc
qainfotech <qainfotech@asda1712po02529.qait.com>
2017-07-13 11:25:33 +0530

    updated

--- src/test/java/web_service_test/assignment_snl_services/NewTest.java
+++ src/test/java/web_service_test/assignment_snl_services/NewTest.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
 import static org.assertj.core.api.Assertions.*;
@@ -35,6 +36,7 @@ public class NewTest {
 
 	int Board_id;
 	JSONObject json_verify;
+	int player_id;
 
 	@Test
 	public void init() {
@@ -90,31 +92,104 @@ public class NewTest {
 
 	}
 
+	static HttpURLConnection conn;
+
 	@Test(dependsOnMethods = "check_board_creation")
 	public void player_add_check() throws IOException, ParseException {
 
-		String message = "{\"board\":\"" + Board_id + "\",\"player\":{\"name\": \"akshay7\"}}";
+		JSONObject jsonobj, jsonplayer;
 
-		HttpURLConnection conn = this.addplayer_post_call(Board_id + "", "akshay");
+		conn = this.addplayer_post_call(Board_id + "", "akshay");
 		assertThat(conn.getResponseCode()).isEqualTo(200);
-
 		conn = this.addplayer_post_call(Board_id + "", "nishant");
 
-		System.out.println(getJson(conn));
+		jsonobj = (JSONObject) getJson(conn).get("response");
+		assertThat(jsonobj).isNotNull();
+		assertThat(Integer.parseInt(jsonobj.get("status").toString())).isEqualTo(1);
+
+		jsonplayer = (JSONObject) jsonobj.get("player");
+		assertThat(Integer.parseInt(jsonplayer.get("board_id").toString())).isEqualTo(Board_id);
+		assertThat(Integer.parseInt(jsonplayer.get("position").toString())).isEqualTo(0);
+		assertThat(jsonplayer.get("name")).isEqualTo("nishant");
+
+		player_id = (Integer.parseInt(jsonplayer.get("id").toString()));
 
 		conn = this.addplayer_post_call(Board_id + "", "shabaash");
 
-		System.out.println(getJson(conn));
+		jsonobj = (JSONObject) getJson(conn).get("response");
+		jsonplayer = (JSONObject) jsonobj.get("player");
+		assertThat(jsonplayer.get("id").toString()).isEqualTo(player_id + 1 + "");
+
 		conn = this.addplayer_post_call(Board_id + "", "shadaab");
+		// assertThat(getJson(conn).get("response")).isNotNull();
+		jsonobj = (JSONObject) getJson(conn).get("response");
+		jsonplayer = (JSONObject) jsonobj.get("player");
+		assertThat(jsonplayer.get("id").toString()).isEqualTo(player_id + 2 + "");
 
-		System.out.println(getJson(conn));
 		conn = this.addplayer_post_call(Board_id + "", "pankaj");
-
-		System.out.println(getJson(conn));
+		jsonobj = (JSONObject) getJson(conn).get("response");
+		jsonplayer = (JSONObject) jsonobj.get("player");
+		assertThat(jsonplayer.get("id").toString()).isEqualTo((player_id + 3 + ""));
+		// assertThat(getJson(conn).get("response")).isNotNull();
 
 	}
 
-	@Test
+	@Test(dependsOnMethods = "player_add_check")
+	public void check_player_details() throws IOException, ParseException {
+
+		URL url = new URL("http://10.0.1.86/snl//rest/v1/player/" + player_id + ".json");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		output = br.readLine();
+
+		System.out.println(output);
+		// test2
+		assertThat(output).isNotBlank();
+
+		JSONObject complete = new JSONObject();
+		JSONParser parser = new JSONParser();
+
+		// test3
+		complete = (JSONObject) parser.parse(output);
+		JSONObject response = (JSONObject) complete.get("response");
+		assertThat(response).isNotNull();
+
+		JSONObject player = (JSONObject) response.get("player");
+		assertThat(Integer.parseInt(player.get("board_id").toString())).isEqualTo(Board_id);
+
+	}
+
+	@Test(dependsOnMethods = "check_player_details")
+	void update_player() throws IOException, ParseException {
+
+		conn = player_modified("nishant sharma", player_id);
+
+		JSONObject complete = getJson(conn);
+
+		JSONObject response = (JSONObject) complete.get("response");
+		assertThat(Integer.parseInt(response.get("status").toString())).isEqualTo(1);
+		assertThat(response).isNotNull();
+
+		JSONObject player = (JSONObject) response.get("player");
+
+		assertThat(player.get("name")).isEqualTo("nishant sharma");
+		assertThat(Integer.parseInt(player.get("board_id").toString())).isEqualTo(Board_id);
+		assertThat(Integer.parseInt(player.get("id").toString())).isEqualTo(player_id);
+
+	}
+
+	@Test(dependsOnMethods = "update_player")
+	void delete_player() {
+
+	}
+
+	@Test(dependsOnMethods = "update_player")
 	public void deleteboard() throws IOException {
 
 		try {
@@ -125,7 +200,11 @@ public class NewTest {
 			conn.setDoOutput(true);
 			conn.connect();
 
-			System.out.println(conn.getResponseCode());
+			assertThat(conn.getResponseCode()).isEqualTo(200);
+			JSONObject obj= getJson(conn);
+			System.out.println("obj "+obj);
+			
+
 
 			conn.disconnect();
 
@@ -146,6 +225,33 @@ public class NewTest {
 
 			connection.setDoOutput(true);
 			connection.setRequestMethod("POST");
+
+			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+
+			writer.write(message);
+
+			writer.close();
+
+			return connection;
+		} catch (IOException e) {
+			// ...
+		}
+		return null;
+
+	}
+
+	HttpURLConnection player_modified(String name, int player_id) {
+
+		String message = "{\"player\":{\"name\": \"" + name + "\"}}";
+		try {
+			// instantiate the URL object with the target URL of the resource to
+			// request
+			URL url = new URL("http://10.0.1.86/snl/rest/v1/player/" + player_id + ".json");
+
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+			connection.setDoOutput(true);
+			connection.setRequestMethod("PUT");
 
 			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
 
